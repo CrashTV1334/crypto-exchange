b246d24084ccbaadbab24fb5adb49a65f80728be
SlokAks <slok.aks204@gmail.com>
2022-03-31 13:05:31 +0530

    Deployed on rinkeby using Alchemy staging key

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
 module.exports = {
   solidity: "0.8.10",
@@ -7,5 +8,11 @@ module.exports = {
     sources: "./src/backend/contracts",
     cache: "./src/backend/cache",
     tests: "./src/backend/test",
+  },
+  networks: {
+    rinkeby: {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
 };
